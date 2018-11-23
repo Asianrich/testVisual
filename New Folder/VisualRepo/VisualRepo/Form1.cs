58c5ccab07fcb036ff5801081d644ed1e4b49282
Richard <manhkhang@gmx.de>
2018-11-23 15:57:29 +0100

    Test

--- New Folder/VisualRepo/VisualRepo/Form1.cs
+++ New Folder/VisualRepo/VisualRepo/Form1.cs
@@ -16,5 +16,10 @@ namespace VisualRepo
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "test";
+        }
     }
 }
