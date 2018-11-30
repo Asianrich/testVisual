6c55a6822dfc5697962950349e5e53e2e05d25fd
Richard <manhkhang@gmx.de>
2018-11-30 13:49:14 +0100

    parallel processing

--- New Folder/VisualRepo/VisualRepo/Form1.Designer.cs
+++ New Folder/VisualRepo/VisualRepo/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -49,11 +51,31 @@
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 1;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(161, 185);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(510, 186);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(134, 115);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -67,6 +89,8 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- New Folder/VisualRepo/VisualRepo/Form1.cs
+++ New Folder/VisualRepo/VisualRepo/Form1.cs
@@ -17,9 +17,51 @@ namespace VisualRepo
             InitializeComponent();
         }
 
+        private delegate void Synchro(string text);
+
+        private void updateTask(string text)
+        {
+            if(richTextBox1.InvokeRequired)
+            {
+                richTextBox1.Invoke(new Synchro(updateTask), text);
+            }
+            else
+            {
+                 richTextBox1.Text += text;
+            }
+
+            
+
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             textBox1.Text = "test";
+        }
+
+
+        bool isstream = false;
+        private async void button2_Click(object sender, EventArgs e)
+        {
+
+            if(!isstream)
+            {
+                isstream = true;
+                await looptask();
+            }
+            else
+            {
+                isstream = false;
+            }
+
+        }
+
+        private async Task looptask()
+        {
+            while(isstream)
+            {
+                await Task.Run( () => updateTask(textBox1.Text));
+            }
         }
     }
 }
